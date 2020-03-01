1cdabefcb0d7e232780267c1b2c66cc7c73c7e04
Dabombber <gallopily@gmail.com>
2020-03-01 18:11:56 +1300

    Download syntax file which matches nano version

--- scripts/install/nansy.sh
+++ scripts/install/nansy.sh
@@ -9,17 +9,31 @@ else
 		echo "$LANGUAGES"
 	else
 		[ "$1" != 'all' ] && LANGUAGES="$*"
+
+		VERSION="$(nano -V | awk 'NR==1{print $NF}')"
+		IFS=' .' read -r MAJOR MINOR PATCH <<-EOF
+			$VERSION
+		EOF
+		PATCH="${PATCH%pre?}"
+		if [ $((${MAJOR:-0} * 10000 + ${MINOR:-0} * 100 + ${PATCH:-0})) -lt 20704 ]; then
+			URL='https://git.savannah.gnu.org/cgit/nano.git/plain/doc/syntax'
+		else
+			URL='https://git.savannah.gnu.org/cgit/nano.git/plain/syntax'
+		fi
+
 		echo 'Downloading...'
-		curl -fs -w '\t%{filename_effective}\n' "https://git.savannah.gnu.org/cgit/nano.git/plain/syntax/{${LANGUAGES// /,}}.nanorc" --create-dirs -o '/jffs/configs/nano/#1.nanorc'
+		curl -fs -w '\t%{filename_effective}\n' "$URL/{${LANGUAGES// /,}}.nanorc?h=v$VERSION" --create-dirs -o '/jffs/configs/nano/#1.nanorc'
+
+		if [ -f /jffs/configs/nano/sh.nanorc ] && ! grep -qF '/jffs/configs/profile\.add' /jffs/configs/nano/sh.nanorc; then
+			escape() { printf '%s\n' "$1" | sed 's/[]\/$*.^&[]/\\&/g'; }
+
+			sed -i -e "s/^\(syntax \"\?sh\"\? \).*$/\1$(escape '"(\.sh|(\.|/)(a|ba|c|da|k|mk|pdk|tc|z)sh(rc|_profile)?|/(etc/|\.)profile|/jffs/configs/profile\.add)$"')/" \
+				-e "s/^header .*$/$(escape 'header "^#!.*/(((a|ba|c|da|k|mk|pdk|tc|z)?sh)|(busybox|env) +sh|openrc-run|runscript)"')/" \
+				-e "s/^magic .*$/$(escape 'magic "(POSIX|Bourne-Again) shell script.*text"')/" \
+				/jffs/configs/nano/sh.nanorc
+		fi
 		if [ ! -f '/jffs/configs/nanorc' ] || ! grep -qF 'include "/jffs/configs/nano/*.nanorc"' /jffs/configs/nanorc; then
 			echo 'include "/jffs/configs/nano/*.nanorc"' >>/jffs/configs/nanorc
-		fi
-		IFS=' .' read -r MAJOR MINOR _ <<-EOF
-			$(nano -V | awk 'NR==1{print $NF}')
-		EOF
-		[ -f /jffs/configs/nano/sh.nanorc ] && ! grep -qF '/jffs/configs/profile\.add' /jffs/configs/nano/sh.nanorc && sed -i 's/^\(syntax sh .*\))\$"$/\1|\/jffs\/configs\/profile\\\.add)\$"/' /jffs/configs/nano/sh.nanorc
-		if [ $((${MAJOR:-0} * 100 + ${MINOR:-0})) -lt 406 ]; then
-			sed -i 's/^formatter/#formatter/' /jffs/configs/nano/*
 		fi
 	fi
 fi
